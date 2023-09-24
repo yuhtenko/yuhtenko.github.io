c06912643538a817179425dbc98a530b15834f8e
Tim Voronov <ziflex@gmail.com>
2023-09-24 14:04:43 -0400

    Added GA

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,6 +1,8 @@
 import type { GatsbyConfig } from 'gatsby';
 import data from './website.json';
 
+const url = new URL(data.url);
+
 const config: GatsbyConfig = {
     siteMetadata: {
         title: data.title,
@@ -42,6 +44,37 @@ const config: GatsbyConfig = {
                 autoLabel: 'dev-only',
                 labelFormat: `[local]`,
                 cssPropOptimization: true,
+            },
+        },
+        {
+            resolve: `gatsby-plugin-google-gtag`,
+            options: {
+                // You can add multiple tracking ids and a pageview event will be fired for all of them.
+                trackingIds: [
+                    'G-LLBZ8T4E9H', // Google Analytics / GA
+                    // 'AW-CONVERSION_ID', // Google Ads / Adwords / AW
+                    // 'DC-FLOODIGHT_ID', // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+                ],
+                // This object gets passed directly to the gtag config command
+                // This config will be shared across all trackingIds
+                // gtagConfig: {
+                //     optimize_id: 'OPT_CONTAINER_ID',
+                //     anonymize_ip: true,
+                //     cookie_expires: 0,
+                // },
+                // This object is used for configuration specific to this plugin
+                pluginConfig: {
+                    // Puts tracking script in the head instead of the body
+                    head: false,
+                    // Setting this parameter is also optional
+                    respectDNT: true,
+                    // Avoids sending pageview hits from custom paths
+                    exclude: [],
+                    // Defaults to https://www.googletagmanager.com
+                    origin: url.hostname,
+                    // Delays processing pageview events on route update (in milliseconds)
+                    delayOnRouteUpdate: 0,
+                },
             },
         },
     ],
